5a82990c0f97dfd6b17e30bbbc16de6d371a4ade
dhshopdelivery <bassirouhassan191@gmail.com>
2026-09-20 12:03:22 +0100

    Upgrade service worker to version 5
    Updated service worker to version 5 and modified cache assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
-const C="dh-shop-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css?v=1","./app.js?v=1","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C = "dh-shop-v5";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(C).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css?v=2",
+        "./app.js?v=5",
+        "./manifest.json"
+      ])
+    )
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== C)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
